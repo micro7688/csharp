08740f9f3ddb0aaad41120c16b0ce775fe8cdc5f
원종 신 <micro7688@naver.com>
2018-10-27 12:14:32 +0900

    C#Day1

---
+++ Day1_1/Day1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day1_2
+{
+    class MyConstants
+    {
+        public const double Pl = 3.14;
+        public const int MYAGE = 22;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 2;
+            double area = MyConstants.Pl * (radius);
+            Console.WriteLine("Area = {0}, Age={1}", area, MyConstants.MYAGE);
+            //MyConstants.MYAGE = 20;   //오류
+
+            const string name = "홍길동";
+            Console.WriteLine("name : " + name);
+
+            //name = "김길동";  //오류
+        }
+    }
+}
